474cb294137f40ab1bc95c8bc0e8b52c36869826
Nicola <nicola.porta00@gmail.com>
2020-12-11 16:32:31 +0100

    Milestone2

--- js/script.js
+++ js/script.js
@@ -3,61 +3,102 @@ var app = new Vue(
     el: "#root",
     // DATA ELEMENT
     data: {
-      list: [
-        "leggere la traccia dell'esercizio",
-        "impostare link e script della pagina HTML",
-        "programmare la pagina Jscript",
-        "fare il refactoring",
-        "pushare l'esercizio",
-        "riposare con una birra"
+      contactsList: [
+        {
+      		name: 'Claudia',
+      		avatar: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_female_woman_avatar-512.png',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Hai portato a spasso il cane?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Ricordati di dargli da mangiare',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 16:15:22',
+      				text: 'Tutto fatto!',
+      				status: 'received'
+      			}
+          ],
+          lastAccess: 'Ultimo accesso 10/01/2020 18:49'
+        },
+        {
+      		name: 'Fabio',
+      		avatar: 'https://www.popsci.com/resizer/oBw2zifFCqH3deU6vy2bPRSG99Q=/760x456/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/WMD5M52LJFBEBIHNEEABHVB6LA.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '20/03/2020 16:30:00',
+      				text: 'Ciao come stai?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '20/03/2020 16:30:55',
+      				text: 'Bene grazie! Stasera ci vediamo?',
+      				status: 'received'
+      			},
+      			{
+      				date: '20/03/2020 16:35:00',
+      				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+      				status: 'sent'
+      			}
+      		],
+          lastAccess: 'Ultimo accesso 15/01/2020 19:45'
+      	},
+      	{
+      		name: 'Samuele',
+      		avatar: 'https://image.freepik.com/premium-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '28/03/2020 10:10:40',
+      				text: 'La Marianna va in campagna',
+      				status: 'received'
+      			},
+      			{
+      				date: '28/03/2020 10:20:10',
+      				text: 'Sicuro di non aver sbagliato chat?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '28/03/2020 16:15:22',
+      				text: 'Ah scusa!',
+      				status: 'received'
+      			}
+      		],
+          lastAccess: 'Ultimo accesso 09/01/2020 15:50'
+      	},
+      	{
+      		name: 'Luisa',
+      		avatar: 'https://image.freepik.com/premium-vector/woman-avatar-profile-round-icon_24640-14042.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Lo sai che ha aperto una nuova pizzeria?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Si, ma preferirei andare al cinema',
+      				status: 'received'
+      			}
+      		],
+          lastAccess: 'Ultimo accesso 12/01/2020 14:27'
+      	},
       ],
-      newElement:"",
-      deleteState:"",
-      search: "",
-      listFiltered:""
+      indexContact: 0
     },
     created: function() {
-      this.list.sort();
       this.listFiltered = this.list;
     },
     // FUNCTIONS
     methods: {
-      // SetTimeOut random
-      removeRandom: function() {
-        setTimeout(
-          () => {
-            this.listFiltered.splice(this.getRandomNumber(app.listFiltered.length),1);
-          }
-        ,3000);
-      },
-      // Random Number Generator
-      getRandomNumber: function(index) {
-        return Math.floor(Math.random() * (index - 1));
-      },
-      // add element on list
-      addNewElement: function() {
-        if(this.newElement != "") {
-          this.deleteState = "";
-          if(this.list[0] == "NESSUNA ATTIVITA' IN PROGRAMMA") {
-            this.list.splice(0,1);
-          }
-          this.listFiltered.push(this.newElement);
-          this.newElement = "";
-          this.listFiltered.sort();
-        }
-      },
-      // remove element on list
-      remove: function(index) {
-        this.listFiltered.splice(index,1);
-        if (this.list.length == 0) {
-          this.list[index] = "NESSUNA ATTIVITA' IN PROGRAMMA";
-          this.deleteState = "delete";
-        }
-      },
-      // delete all element on list
-      clearList: function() {
-        this.listFiltered = [];
-      },
       // filter in list
       filterList: function() {
         if (this.search != "") {
